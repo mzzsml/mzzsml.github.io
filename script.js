82b1508f3c4574dfc80aa6b8f63463cff30f7763
mzzsml <mzzsml@protonmail.ch>
2018-09-19 20:47:23 +0200

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+let d,h,m;var timeout=100;window.onload=function(){setInterval(timeNow,timeout)}
+function addZero(i){if(i<10){i="0"+i}
+return i}
+function timeNow(){d=new Date();h=addZero(d.getHours());m=addZero(d.getMinutes());document.getElementById("clock").innerHTML=h+':'+m}
+$(document).ready(function(){$(document).on("mousedown",function(e){if(e.button==2){$('body').toggleClass('dark');$('.container').toggleClass('dark');$('.clock-outer').toggleClass('dark');$('.links').toggleClass('dark');$('h1').toggleClass('dark');$('a:link').toggleClass('dark');$('a:visited').toggleClass('dark');$('.clock-inner').toggleClass('dark');return!1}
+return!0})})
